29ffbc8b08b38dcc1dd74fe6cb8fb370e7bf643b
MM10 <mm33007@outlook.com>
2023-07-03 12:39:29 +0330

    created admin.py for core and user

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from apps.core.admin import BaseAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+User = get_user_model()
+
+
+class UserAccountAdmin(BaseAdmin, admin.ModelAdmin):
+    list_display = ('username', 'phone_number', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('username', 'phone_number')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('phone_number',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'phone_number', 'is_staff', 'is_active')}
+         ),
+    )
+
+
+admin.site.register(User, UserAccountAdmin)
